4e577cdc264d1e1a35503a2d74a67bc05efe9163
Gagan <gagankumar6696@gmail.com>
2024-08-20 12:22:54 +0530

    added

---
+++ app_engine/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World from github666!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
