09028dabe190e4af82fce7a8632001a5e978c7d9
Gagan-R31 <167845584+Gagan-R31@users.noreply.github.com>
2024-08-20 14:33:20 +0530

    Update main.py

--- cloud_functions/general_api/main.py
+++ cloud_functions/general_api/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World from cloud functions!"
+    return "Hello, World from cloud functiondsdss!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
